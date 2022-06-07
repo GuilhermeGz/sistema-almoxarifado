458abe25b80b2fe9e030758060f2655ca936250c
Guilherme Silva <guilherme.dna06@gmail.com>
2022-06-07 14:20:44 -0300

    Migração de recibo

---
+++ database/migrations/2022_06_07_140529_create_recibo_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReciboTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recibo', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('itens');
+            $table->unsignedInteger('unidade_id')->index();
+            $table->foreign('unidade_id')->references('id')->on('unidades');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recibo');
+    }
+}
