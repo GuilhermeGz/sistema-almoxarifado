b45d015d0b7994db0597deedbd9d7218bb6f1215
Edgarvital <edgar.tecnico@hotmail.com>
2022-09-10 15:46:38 -0300

    Atrelando o usuario solicitante a unidade

--- resources/views/unidade/unidade_create.blade.php
+++ resources/views/unidade/unidade_create.blade.php
@@ -1,6 +1,8 @@
 @extends('templates.principal')
 
-@section('title') Cadastrar Unidade Basica @endsection
+@section('title')
+    Cadastrar Unidade Basica
+@endsection
 
 @section('content')
     <div style="border-bottom: #949494 2px solid; padding: 5px; margin-bottom: 10px">
@@ -50,6 +52,57 @@
             </div>
         </div>
 
+        <div class="form-group">
+            <h2 class="h4"> Dados do Responsável/Login</h2>
+        </div>
+
+        <div class="form-group row">
+            <div class="col-md-4">
+                <label for="email"> E-mail </label>
+                <input class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}"
+                       autocomplete="email" autofocus type="email" name="email" id="email"
+                       placeHolder="exemplodeemail@upe.br">
+                @error('email')
+                <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+
+            <div class="col-md-4">
+                <label for="numTel">{{ __('Telefone') }}</label>
+                <input id="numTel" type="text" min="0" class="form-control @error('numTel') is-invalid @enderror"
+                       name="numTel" value="{{ old('numTel') }}" required autocomplete="numTel"
+                       placeholder="(00)00000-0000">
+
+                @error('numTel')
+                <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                @enderror
+            </div>
+
+            <div class="col-md-4">
+                <label for="cpf"> CPF </label>
+                <input type="text" name="cpf" id="cpf" class="form-control @error('cpf') is-invalid @enderror"
+                       value="{{ old('cpf') }}" autocomplete="cpf" autofocus placeHolder="000.000.000-00">
+                @error('cpf')
+                <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                @enderror
+            </div>
+
+
+            <input hidden class="form-control @error('senha') is-invalid @enderror" autofocus
+                   autocomplete="new-password"
+                   type="password" name="password" id="password" placeHolder="" value="almoxarifado123">
+            <input hidden class="form-control @error('confirmar_senha') is-invalid @enderror"
+                   autocomplete="new-password"
+                   autofocus type="password" name="password_confirmation" id="password_confirmation" placeHolder=""
+                   value="almoxarifado123">
+        </div>
+
         <div class="form-group row">
             <div class="col-md-4">
                 <Button class="btn btn-secondary" type="button"
@@ -63,5 +116,5 @@
 @endsection
 
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-<script type="text/javascript" src="{{asset('js/material/CheckFields.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/unidade/CheckFields.js')}}"></script>
 <script type="text/javascript" src="{{asset('js/unidade/busca.js')}}"></script>
