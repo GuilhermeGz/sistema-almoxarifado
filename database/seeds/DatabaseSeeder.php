e8d9df00d51513d1284520d4c256b7f64ba9234c
GuilhermeGz <guilherme.dna06@gmail.com>
2022-09-20 14:58:56 -0300

    Retirada da geração de alguns seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,14 +12,14 @@ class DatabaseSeeder extends Seeder
         $this->call(CargoSeeder::class);
         $this->call(UsuarioSeeder::class);
         $this->call(DepositoSeeder::class);
-        $this->call(SetorSeeder::class);
-        $this->call(UnidadeSeeder::class);
-        $this->call(MaterialSeeder::class);
-        $this->call(EstoqueSeeder::class);
-        $this->call(OrdemFornecimentoSeeder::class);
-        $this->call(EmitenteSeeder::class);
-        $this->call(NotaFiscalSeeder::class);
-        $this->call(MaterialNotasSeeder::class);
+        //$this->call(SetorSeeder::class);
+        //$this->call(UnidadeSeeder::class);
+        //$this->call(MaterialSeeder::class);
+        //$this->call(EstoqueSeeder::class);
+        //$this->call(OrdemFornecimentoSeeder::class);
+        //$this->call(EmitenteSeeder::class);
+        //$this->call(NotaFiscalSeeder::class);
+        //$this->call(MaterialNotasSeeder::class);
         //$this->call(NotificacaoSeeder::class);
     }
 }
--- database/seeds/UsuarioSeeder.php
+++ database/seeds/UsuarioSeeder.php
@@ -12,7 +12,7 @@ class UsuarioSeeder extends Seeder
     public function run()
     {
         factory(\App\Usuario::class, 1)->create(['email' => 'admin@admin.teste', 'cargo_id' => '1', 'nome' => 'administrador']);
-        factory(\App\Usuario::class, 1)->create(['email' => 'diretoria@admin.teste', 'cargo_id' => '2', 'nome' => 'diretoria']);
-        factory(\App\Usuario::class, 1)->create(['email' => 'solicitante@admin.teste', 'cargo_id' => '3', 'nome' => 'solicitante']);
+        //factory(\App\Usuario::class, 1)->create(['email' => 'diretoria@admin.teste', 'cargo_id' => '2', 'nome' => 'diretoria']);
+        //factory(\App\Usuario::class, 1)->create(['email' => 'solicitante@admin.teste', 'cargo_id' => '3', 'nome' => 'solicitante']);
     }
 }
